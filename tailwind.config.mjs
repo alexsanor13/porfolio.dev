16e533a25180b1c77bc5889f60127194c9d0199b
alexsanor13 <alexsanor13@gmail.com>
2024-12-07 13:20:19 +0100

    New avatar and description

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'radial-light':
+					'radial-gradient(ellipse 80% 80% at 50% -20%, rgba(217,216,255,0.5), rgba(255,255,255,0.9))',
+				'radial-dark':
+					'radial-gradient(ellipse 80% 80% at 50% -20%, rgba(120,119,198,0.3), rgba(255,255,255,0))',
+			},
+		},
 	},
 	plugins: [],
 }
